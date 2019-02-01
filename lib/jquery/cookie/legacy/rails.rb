4c0f6a4180f220b8e143d9148c3adeca2840cd8e
Toshiyuki HINA <thina@yourinventit.com>
2019-02-01 16:14:03 +0900

    initial commit

---
+++ lib/jquery/cookie/legacy/rails.rb
@@ -0,0 +1,12 @@
+require "jquery/cookie/legacy/rails/version"
+
+module Jquery
+  module Cookie
+    module Legacy
+      module Rails
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/jquery/cookie/legacy/rails/version.rb
@@ -0,0 +1,9 @@
+module Jquery
+  module Cookie
+    module Legacy
+      module Rails
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
